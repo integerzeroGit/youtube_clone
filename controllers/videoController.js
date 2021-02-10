ec4018c7c713ec6d2ccef8336f693f9e503fdb06
integerzeroGit <favoritemail.integerzero@gmail.com>
2021-02-10 15:54:29 +0900

    create search controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,16 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("Search");
-export const videos = (req, res) => res.render("Videos");
-export const upload = (req, res) => res.render("home");
-export const videoDetail = (req, res) => res.render("Video Detail");
-export const editVideo = (req, res) => res.render("Edit Video");
-export const deleteVideo = (req, res) => res.render("Delete Video");
+export const home = (req, res) => res.render("home", { pageTitle: "Home"});
+export const search = (req, res) => {
+    const {query:{ term: searchingBy }} = req;
+    // const searchingBy = req.query.term;
+    // 위의 두 개는 완전히 같은 결과
+    console.log(req.query);
+    res.render("search", { pageTitle: "Search", searchingBy });
+}
+export const upload = (req, res) =>
+    res.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) =>
+    res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) =>
+    res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) =>
+    res.render("deleteVideo", { pageTitle: "Delete Video" });
